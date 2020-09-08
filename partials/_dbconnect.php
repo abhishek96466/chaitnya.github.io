677ef7730a55d5453724a175a82511dc08b5784f
Abhishek <sharma.abhisheky2@gmail.com>
2020-09-08 11:21:31 +0530

    Update _dbconnect.php

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
-$server="sql309.epizy.com";
-$username="	epiz_26689747";
-$password="3Yn7vacDrI";
-$database="epiz_26689747_chaitnya";
+$server="localhost";
+$username="root";
+$password="";
+$database="chaitnya";
 
 $conn=mysqli_connect($server,$username,$password,$database);
 
